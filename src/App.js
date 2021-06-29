7f4e2135bf614d6b9a5abeb67dcabc055c227e4e
Gabriel-Floricel <gabrielfloricel@yahoo.com>
2021-06-29 12:32:58 +0300

    pentru brO

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
-import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [parking, setParking] = useState(5);
+  const [licensePlate, setLicensePlate] = useState("");
+  const [parkSpaces, setParkSpaces] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setParkSpaces((current) => [...current, licensePlate.toUpperCase()]);
+    setParking((current) => current - 1);
+  };
+
+  const handleChange = (event) => {
+    // event.persist();
+    setLicensePlate(event);
+  };
+
+  const handleClick = () => {
+    const array = new Array(parkSpaces);
+    console.log(array);
+    for (let i = 0; i < array.length; i++) {
+      if (licensePlate.toUpperCase() === array[i]) {
+        array.splice(i, 1);
+      }
+    }
+    console.log(array);
+  };
+
   return (
     <div>
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
+      <section className='main'>
+        <h1>Parking spaces available: {parking} </h1>
+        <h2>Currently occupied parking spaces:</h2>
+        <ol>
+          {parkSpaces.map((e, index) => (
+            <li key={index}>{e}</li>
+          ))}
+        </ol>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='park'>Enter your license plate number</label>
+            <br />
+            <input
+              id='park'
+              type='text'
+              value={licensePlate}
+              onChange={(e) => handleChange(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <input
+              disabled={parking === 0}
+              type='submit'
+              className='btn btn-primary'
+              value='Enter parking'
+            />
+          </div>
+
+          <div>
+            <input
+              type='button'
+              className='btn btn-danger'
+              value='Exit parking'
+              htmlFor='park'
+              onClick={handleClick}
+            />
+          </div>
+          <p role='alert'>{parking === 0 && "The parking is full!"}</p>
+        </form>
+      </section>
     </div>
   );
 }
